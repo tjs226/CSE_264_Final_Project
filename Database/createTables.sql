3e9055a0bde7e961e917e916d399e5894a8d0a71
Tedd Stabolepszy <143549208+tjs226@users.noreply.github.com>
2025-11-11 09:26:01 -0500

    Create users and events tables in database
    This SQL script creates the 'users' and 'events' tables for the project database, defining their structure and relationships.

---
+++ Database/createTables.sql
@@ -0,0 +1,29 @@
+-- run this file to create the tables in the database used for the project
+
+--users table
+CREATE TABLE users(
+  id SERIAL NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  hashed_password TEXT NOT NULL,
+  salt TEXT NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+-- events table 
+CREATE TABLE events(
+  id SERIAL NOT NULL,
+  owner BIGINT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  date VARCHAR(255) NOT NULL,
+  start_time VARCHAR(255),
+  end_time VARCHAR(255),
+  location VARCHAR(255) NOT NULL,
+  description TEXT,
+  rsvp_amount BIGINT DEFAULT 0,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE
+);
