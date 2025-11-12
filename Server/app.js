4ef6687a75c00242cf66fdc3969fbffc6008fb67
Tedd Stabolepszy <143549208+tjs226@users.noreply.github.com>
2025-11-12 17:47:51 -0500

    added events route

--- Server/app.js
+++ Server/app.js
@@ -11,6 +11,20 @@ const port = 3000
 // make home route
 app.get('/', (request, response) => {
     response.send("Bethlehem Events API")
+});
+
+
+// GET route to return all events
+app.get('/events', async (request, response) =>  {
+    const sql = "SELECT * FROM tta_events";
+
+    try {
+        const data = await query(sql, []);
+        response.json(data.rows);
+    } catch (err) {
+        console.log(err);
+        response.status(500).json({error: "Internal Server Error"})
+    }
 })
 
 
